990fbe1e4cf0adc5e1659e51d28df6e4e96084a5
mizdra <pp.mizdra@gmail.com>
2022-03-14 00:31:09 +0900

    use import

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
-import styles from './index.module.less';
+import basicStyle from './basic.module.less';
+import importStyle from './import.module.less';
 
 export const a = 1;
 export const b = 'text';
 
-console.log(styles.someStyles);
-console.log(styles.nestedClass);
-console.log(styles.nestedAnother);
+console.log(basicStyle.someStyles);
+console.log(basicStyle.nestedClass);
+console.log(basicStyle.nestedAnother);
+
+console.log(importStyle.foo);
+console.log(importStyle.bar);
+console.log(importStyle.clearfix);

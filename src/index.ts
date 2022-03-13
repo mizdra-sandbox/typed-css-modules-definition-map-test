6f384fa27bf1a2355b66639b03c27f06ca735b57
mizdra <pp.mizdra@gmail.com>
2022-03-13 22:50:22 +0900

    typed-less-module

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
+import styles from './index.module.less';
+
 export const a = 1;
 export const b = 'text';
+
+console.log(styles.someStyles);
+console.log(styles.nestedClass);
+console.log(styles.nestedAnother);

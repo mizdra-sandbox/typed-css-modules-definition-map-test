dc89b3fb18f40c23f2568b801cbb4a3e084bbf3b
mizdra <pp.mizdra@gmail.com>
2022-03-15 01:05:18 +0900

    add fixtures

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import basicStyle from './less/basic.module.less';
-import importStyle from './less/import.module.less';
 
-export const a = 1;
-export const b = 'text';
-
-console.log(basicStyle.someStyles);
-console.log(basicStyle.nestedClass);
-console.log(basicStyle.nestedAnother);
-
-console.log(importStyle.foo);
-console.log(importStyle.bar);
-console.log(importStyle.clearfix);
+console.log(basicStyle.basic);
+console.log(basicStyle.nesting);
+console.log(basicStyle.nesting_1);
+console.log(basicStyle.nesting_2);
+console.log(basicStyle.cascading);
+console.log(basicStyle.pseudo_class);
+console.log(basicStyle.use_mixin);
+console.log(basicStyle.clearfix);
+console.log(basicStyle.at_rule);
+console.log(basicStyle.selector_list_1);
+console.log(basicStyle.selector_list_2);
+console.log(basicStyle.global_class_name_1);
+console.log(basicStyle.global_class_name_2);

f8ba95ef7a12e3d947d421441280540efb0b6197
mizdra <pp.mizdra@gmail.com>
2022-03-26 01:43:59 +0900

    less package を使う

--- scripts/gen:tcm:default.ts
+++ scripts/gen:tcm:default.ts
@@ -1,20 +1,19 @@
 import DtsCreator from 'typed-css-modules';
-import syntax from 'postcss-less';
-import postcss from 'postcss';
 import { readFile } from 'fs/promises';
-import postcssPresetEnv from 'postcss-preset-env';
-import postcssMixins from 'postcss-mixins';
+import less from 'less';
 
 const path = 'src/less/basic.module.less';
 
 const creator = new DtsCreator();
-const processor = postcss([postcssMixins, postcssPresetEnv({stage: 0}) as any]);
 
 async function main() {
-  const lessText = await readFile(path);
-  const result = await processor.process(lessText, { syntax, from: path });
-  const cssText = result.content;
-  console.log({cssText});
+  const lessText = await readFile(path, 'utf8');
+  const lessRenderOutput = await less.render(lessText, {
+    filename: path,
+    sourceMap: {},
+  });
+  const cssText = lessRenderOutput.css;
+  console.log({ cssText });
 
   const dtsContent = await creator.create(path, cssText);
   console.log(dtsContent.tokens); // ['myClass']
